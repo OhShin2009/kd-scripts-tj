08bfb2ac8b9252b3a02c06bb69278a3791b4c287
frank <18652171101@163.com>
2018-02-24 15:42:32 +0800

    modify

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:~/.nvm/versions/node/v8.9.4/bin;
 export PATH
-
+cd /home/kd-proxy
+git pull
+npm install
+pm2 delete all
+pm2 start /home/proxy.json
