d54293b39bfd7ba84d5259a39d879ad762904370
frank <18652171101@163.com>
2018-03-02 12:26:57 +0800

    modify

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ export PATH
 function deploy_vpn(){
   echo "execute deploy_vpn" >> /home/deploy.log
   if ! [ -x "$(command -v ipsec)" ]; then
-    echo "1"
+    echo "1" >> /home/deploy.log
     cd /home/kd-scripts
     git pull
     bash /home/kd-scripts/create_ikev2_vpn.sh
   else
     ps -fe | grep ipsec | grep -v grep
     if [ $? -ne 0 ] ; then
-      echo "2"
+      echo "2" >> /home/deploy.log
       ipsec start
     else
-      echo "3"
+      echo "3" >> /home/deploy.log
       ipsec restart
     fi
   fi
